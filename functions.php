4c078417676a747c05817f8aa90b3445dc0f4452
Scott Sutherland <scott@we-shape.co.uk>
2015-11-11 22:44:09 +0000

    .

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
 		wp_enqueue_style( 'google-font', "http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700" );
 
 		// Load our main stylesheets.
-		wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . "/css/bootstrap.min.css" );
-		wp_enqueue_style( 'main-style', get_template_directory_uri() . "/css/SM4H_style.css" );
+		wp_enqueue_style( 'bootstrap-style', "https://d138ib3u50nxkx.cloudfront.net/css/bootstrap.min.css" );
+		wp_enqueue_style( 'main-style', "https://d138ib3u50nxkx.cloudfront.net/css/SM4H_style.css" );
 		wp_enqueue_style( 'SM4H-2015-style', get_stylesheet_uri() );
 
 		wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js' );
