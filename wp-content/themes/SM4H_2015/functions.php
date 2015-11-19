5f0514530fa59470c028fd7fe6ab222ff5060894
Scott Sutherland <scott@we-shape.co.uk>
2015-11-19 14:59:30 +0000

    Uploading Files

---
+++ wp-content/themes/SM4H_2015/functions.php
@@ -0,0 +1,22 @@
+<?php 
+
+	// Theme & Javascript Functions
+
+	function SM4H_2015_scripts() {
+
+		// Google Fonts
+		wp_enqueue_style( 'google-font', "http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700" );
+
+		// Load our main stylesheets.
+		wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . "/css/bootstrap.min.css" );
+		wp_enqueue_style( 'main-style', get_template_directory_uri() . "/css/SM4H_style.css" );
+		wp_enqueue_style( 'SM4H-2015-style', get_stylesheet_uri() );
+
+		wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js' );
+	}
+
+	add_action( 'wp_enqueue_scripts', 'SM4H_2015_scripts' );
+
+
+	// Menu Functions
+?>
